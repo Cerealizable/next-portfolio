3c2711e64360ae4bf8276a90b918ee7708ba0f77
Cerealizable <alberstc1@gmail.com>
2022-05-11 11:38:08 -0700

    Feat: Created Banner

--- src/components/header/mobile-drawer.js
+++ src/components/header/mobile-drawer.js
@@ -58,7 +58,7 @@ export default function MobileDrawer() {
             >
               {menuItem.label}
             </Link>
-            ))};
+            ))}
         </Box>
         <Box sx={styles.menuFooter}>
             <Box sx={styles.social}>
