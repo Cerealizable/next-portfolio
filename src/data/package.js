d4d8443f3c4b39dfc276078108743426df2e6127
Cerealizable <alberstc1@gmail.com>
2022-05-13 08:33:43 -0700

    Feat: plans and plan cards created

--- src/data/package.js
+++ src/data/package.js
@@ -8,6 +8,8 @@ import ButtonGroup from 'components/button-group';
 import SectionHeader from 'components/section-header';
 import { IoIosCheckmarkCircle, IoIosCloseCircle } from 'react-icons/io';
 
+// separate the data from the representation in future
+
 const packages = {
   monthly: [
     {
@@ -241,17 +243,6 @@ const responsive = {
 
 export default function Package() {
   const { monthly, annual } = packages;
-  const [state, setState] = useState({
-    active: 'monthly',
-    pricingPlan: monthly,
-  });
-  const handlePricingPlan = (plan) => {
-    if(plan == 'annual'){
-      setState({ active: 'annual', pricingPlan: annual, });
-    } else {
-      setState({ active: 'monthly', pricingPlan: monthly,})
-    }
-  };
 
   const sliderParams = {
     additionalTransfrom: 0,
@@ -277,43 +268,7 @@ export default function Package() {
   };
 
   return (
-    <section id="pricing" sx={{variant: 'section.pricing'}}>
-      <Container>
-        <SectionHeader 
-          slogan="Pricing Plan"
-          title="Choose your pricing plan"
-        />
-        <Flex sx={styles.buttonGroup}>
-          <Box sx={styles.buttonGroupInner}>
-            <button
-              className={state.active == 'monthly' ? 'active' : ''}
-              type="button"
-              aria-label='Monthly'
-              onClick={() => handlePricingPlan('monthly')}
-            >
-              Monthly Plan
-            </button>
-            <button
-              className={state.active == 'annual' ? 'active' : ''}
-              type="button"
-              aria-label='Annual'
-              onClick={() => handlePricingPlan('annual')}
-            >
-              Annual Plan
-            </button>
-          </Box>
-        </Flex>
-        <Box sx={styles.pricingWrapper} className="pricing__wrapper">
-          <Carousel {...sliderParams}>
-             {state.pricingPlan.map((packageData) => (
-               <Box sx={styles.pricingItem} key={packageData.id}>
-                 <PriceCard data={packageData} />
-               </Box>
-             ))}
-          </Carousel>
-        </Box>
-      </Container>
-    </section>
+    <h1>Package</h1>
   );
 }
 
